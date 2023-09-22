3bf91b9bab81b566ebf19f1b613644ff769d144a
Binu Peter <binupeterp07@gmail.com>
2023-09-22 16:56:04 +0100

    Completed the printing Section of Employee App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import java.math.RoundingMode
 
 val firstName = "Joe"
 val surName = "Soap"
@@ -9,6 +10,34 @@ val prsiPercentage = 5.2f
 val annualBonusAmount = 1450.50f
 val cycletoWorkSchemeMonthlyDeduction = 54.33f
 
+fun printingPayslip(){
+    val monthlySalary = grossSalary/12
+    val payee = payePercentage * monthlySalary/100
+    val bonus = annualBonusAmount/12
+    val prsi = monthlySalary*prsiPercentage/100
+
+    val gross = grossSalary/12
+    val monthlyDeduction = payee+prsi+cycletoWorkSchemeMonthlyDeduction
+    val netpay = gross - monthlyDeduction
+
+    println("-------------------------------------------------------------------")
+    println("| \t \t \t \t \t \t  Monthly Payslip   \t \t \t \t \t  |" )
+    println("|-----------------------------------------------------------------|")
+    println("|\tEmployee Name:${firstName.uppercase()} ${surName.uppercase()}($gender) \t \t Employee Id:$employeeId\t \t \t  |")
+    println("|-----------------------------------------------------------------|")
+    println("|\tPAYMENT DETAILS \t \t \t \t DEDUCTION DETAILS \t \t \t  |")
+    println("|-----------------------------------------------------------------|")
+    println("|\tSalary:%.2f".format(monthlySalary) +"\t \t \t \t \t PAYEE: %.2f".format(payee) +"\t \t \t \t  |")
+    println("|\tBonus: %.2f".format(bonus)+"\t \t \t \t \t PRSI:%.2f".format(prsi) +" \t \t \t \t  |")
+    println("| \t \t \t \t \t \t \t \t  \t Cycle To Work:$cycletoWorkSchemeMonthlyDeduction \t \t  |")
+    println("|-----------------------------------------------------------------|")
+    println("|\tGross:%.2f".format(gross)+"\t \t \t \t \t Total Deduction:%.2f".format(monthlyDeduction)+" \t  |")
+    println("|-----------------------------------------------------------------|")
+    println("|\t \t \t \t \t \tNET PAY: %.2f".format(netpay) +"\t \t \t \t \t \t  |")
+    println("-------------------------------------------------------------------")
+}
+
 fun main(args: Array<String>) {
     println("Pay Slip Printer")
+    printingPayslip()
 }
