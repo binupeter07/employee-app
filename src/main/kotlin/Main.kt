994e196f66414820e0b6e78065bb64114caba28a
Binu Peter <binupeterp07@gmail.com>
2023-09-22 14:20:02 +0100

    Employee Details Provided

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+
+val firstName = "Joe"
+val surName = "Soap"
+val gender = "M"
+val employeeId = "6143"
+val grossSalary = 67543.21f
+val payePercentage = 38.5f
+val prsiPercentage = 5.2
+val annualBonusAmount = 1450.50f
+val cycletoWorkSchemeMonthlyDeduction = 54.33f
+
 fun main(args: Array<String>) {
     println("Pay Slip Printer")
 }
