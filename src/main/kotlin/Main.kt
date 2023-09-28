cb563534847d62ca2e462fa5885afc9786ce75ac
Binu Peter <binupeterp07@gmail.com>
2023-09-28 13:09:14 +0100

    Set numbers for calling specific functions.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -47,8 +47,45 @@ fun getPayslip() =
     -------------------------------------------------------------------""")
 
 
+fun menu(): Int{
+    print("""
+        Employee Menu for ${getFullName()}
+            1. Monthly Salary
+            2. Monthly PRSI
+            3. Monthly PAYE
+            4. Monthly Gross Pay
+            5. Monthly Total Deduction
+            6. Monthly Net Pay
+            7. Full Payslip
+            -1.Exit
+        Enter Option : """
+    )
+    return readLine()!!.toInt()
+
+}
 fun main(args: Array<String>) {
+    var input :Int
+    do {
+        input = menu()
+        when(input){
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 -> println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+
+        }
+    }while (input != -1)
     println(getFullName())
-    println(" Pay Slip Printer")
-    println(getPayslip())
+    println("Monthly Salary: ${getMonthlySalary()}")
+
+
+
+
+
+
 }
